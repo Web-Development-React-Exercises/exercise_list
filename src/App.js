664f19114f9f780235b57b3070704fc68f1170df
PortalFan202 <anibaldonoso1@gmail.com>
2022-09-16 12:39:25 +0300

    Finished Exercise

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useEffect, useState} from 'react';
 
 function App() {
+  const [items, setItems] = useState(['Jouni', 'Pekka', 'Keskuu', 'Dessame'])
+
+  useEffect(() => {
+    console.log(items)
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {items.map(i=>
+          <li key={i}>
+            {i}
+          </li>
+        )}
+      </ul>
     </div>
   );
 }
